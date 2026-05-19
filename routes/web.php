126620ff79103c40c58becc7234c101dbc0828a2
Spy64bit <wengkhai970818@gmail.com>
2026-05-19 17:29:21 +0800

    fix: remove user registration route

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,16 @@ use App\Http\Controllers\Login;
 use App\Http\Controllers\Logout;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\PurchaseOrderController;
-use App\Http\Controllers\Register;
+// use App\Http\Controllers\Register;
 use App\Http\Controllers\StockMovementController;
 use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'guest'], function () {
     Route::inertia('/login', 'Login')->name('login');
-    Route::inertia('/register', 'Register')->name('register');
+    // Route::inertia('/register', 'Register')->name('register');
     Route::post('/login', Login::class)->name('login.post');
-    Route::post('/register', Register::class)->name('register.post');
+    // Route::post('/register', Register::class)->name('register.post');
 });
 
 Route::group(['middleware' => 'auth'], function () {
