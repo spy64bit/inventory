85d78017827d378054aa8345162634a849b30fe4
Spy64bit <wengkhai970818@gmail.com>
2026-08-17 23:48:33 +0800

    fix: fix case sensitive function name

--- app/Filters/CustomerFilter.php
+++ app/Filters/CustomerFilter.php
@@ -4,7 +4,7 @@ namespace App\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 
-class CustomerFIlter extends QueryFilter
+class CustomerFilter extends QueryFilter
 {
     protected function allowedSorts(): array
     {
